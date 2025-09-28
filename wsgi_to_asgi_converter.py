a67f5ca355a7d4f1a98768373f058f649a194fcf
HJ <hjleegcti@gmail.com>
2025-09-28 14:08:18 -0700

    Fix eventlet monkey patching - add eventlet.monkey_patch() before imports

--- wsgi_to_asgi_converter.py
+++ wsgi_to_asgi_converter.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """
-Convert Flask WSGI app to ASGI for WebSocket support
+Convert Flask WSGI app to ASGI for WebSocket support with eventlet monkey patching
 """
+
+import eventlet
+# CRITICAL: Monkey patch must be done before importing any other modules
+eventlet.monkey_patch()
 
 from asgiref.wsgi import WsgiToAsgi
 from app import app
