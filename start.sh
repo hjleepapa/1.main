79f9952c6c52a490ad99a3e45581ea06c0ed8a84
HJ <hjleegcti@gmail.com>
2025-09-28 17:11:39 -0700

    Fix sounddevice dependency and switch to gthread worker to avoid monkey patching issues

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # WebSocket-enabled start script for Render.com
-gunicorn --worker-class gevent -w 1 --bind 0.0.0.0:$PORT start_simple:app
+gunicorn --worker-class gthread -w 1 --threads 4 --bind 0.0.0.0:$PORT start_simple:app
